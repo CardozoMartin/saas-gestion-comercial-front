d3ffa81c559bd8feda0e0311d58d14502d81d349
martin.cardozo@zymo.it <martin.cardozo@zymo.it>
2026-01-21 17:30:51 -0300

    se corrige cors

--- src/api/config.ts
+++ src/api/config.ts
@@ -6,7 +6,7 @@ import { useSession } from "../store/useSession";
 
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:3000/api/v1",
+  baseURL: import.meta.env.VITE_API_BASE_URL || "https://gestion.jhservices.store/api/v1",
   headers: {
     "Content-Type": "application/json",
   },
